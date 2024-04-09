7451c1fecda33d9bbb09cc8548ffd49d8d521f44
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-04-09 10:43:04 -0300

    Create leia.py

---
+++ leia.py
@@ -0,0 +1,10 @@
+num1 = int(input("Digite o primeiro valor inteiro: "))
+num2 = int(input("Digite o segundo valor inteiro: "))
+num3 = int(input("Digite o terceiro valor inteiro: "))
+
+
+valores = [num1, num2, num3]
+valores.sort()
+
+
+print("Valores em ordem crescente:", valores)
