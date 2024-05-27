fcc82e3fa2e67b0855a2c668fb026937d2dedf02
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-05-27 08:59:13 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,11 +1,46 @@
-def Forca(Tentativa):
+def Forca(tentativa):
     f1 = "  +------------+ "
-    f2 = "  |            | "
-    f3 = "  |            O "
-    f4 = "  |           /|\ "
-    f5 = "  |            |  "
-    f6 = "  |           / \ "
+    f2 = "  |              "
+    f3 = "  |              "
+    f4 = "  |              "
+    f5 = "  |              "
+    f6 = "  |              "
     f7 = "__|_______              "
+
+    if tentativa >= 1:
+        f2= "  |            | "
+
+    if tentativa >= 2:
+        f3 = "  |            O " 
+
+    if tentativa >= 3:  
+        f4 = "  |            | "
+
+    if tentativa >= 4: 
+        f4 = "  |           /| "
+
+    if tentativa >= 5: 
+        f4 = "  |           /|\ "
+
+    if tentativa >= 6: 
+        f5 = "  |            |  "
+
+    if tentativa >= 7:
+        f6 = "  |           /  "
+
+    if tentativa >= 8:
+        f6 = "  |           / \ "  
+
+    print(f1)
+    print(f2)
+    print(f3)
+    print(f4)
+    print(f5)
+    print(f6)
+    print(f7)
+
+
+Forca(0)
 
 
 def Continua():
@@ -22,5 +57,10 @@ def Continua():
             print("Digite S ou N: ")
     return Acabou
 Jogar = True
+x=0
+
 while Jogar:
+    print(x)
+    Forca(x)
     Jogar = Continua()
+    x = x + 1
