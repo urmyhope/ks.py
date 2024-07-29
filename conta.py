3351a314ff22f7ae9d9c99f6f9cd36e8f4b1b6db
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-07-29 09:01:19 -0300

    Create conta.py

---
+++ conta.py
@@ -0,0 +1,12 @@
+class conta:
+    def __ini__(Self, numero, titular, saldoinicial, limite):
+        Self.numero = numero
+        Self.titular = titular
+        Self.saldo = saldoinicial
+        Self.limite = limite
+
+
+
+conta1 = conta(134, "Kauany",600.00,50.00)
+
+print(conta1)
