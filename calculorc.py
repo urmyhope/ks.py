0040ce6fb70d8792d9996d92506aeffcaff8c3ec
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-08-20 09:49:53 -0300

    Update calculorc.py

--- calculorc.py
+++ calculorc.py
@@ -11,12 +11,22 @@ def circulo(tipoC,raio):
     c = 0
     if tipoC == "area":
         c = 3.14 + (raio ** 2)
-    elif tipoC == "perimero":
+    elif tipoC == "perimetro":
         c = 2 * 3.14 * (raio)
     return c
 
-ra = retangulo("area", 15, 4.5)
+def verifica_resultado(altura, largura, raio):
+    print(f'retangulo area:{retangulo("area", largura, altura)}')
+    print(f'retangulo perimetro: {retangulo("perimetro", largura, altura)}')
+    print(f'circulo perimetro:{circulo("perimetro", raio )}')
+
+
+
+ra = retangulo("area", 8, 12)
 print(f'area:{ra}')
 
 ca = circulo("area", 5)
 print(f'area: {ca}')
+
+verifica_resultado(8,12,5)
+
